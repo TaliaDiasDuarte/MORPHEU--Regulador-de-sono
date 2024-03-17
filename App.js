bbe8ca9c6e7d0197efc840432b925e1e8a8fdb50
TaliaDiasDuarte <taliadias.adm@gmail.com>
2024-03-17 01:37:04 -0300

    alteracao03

--- App.js
+++ App.js
@@ -1,39 +1,66 @@
 
-import { StyleSheet, Text, View, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
-
-
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import logoImage from './src/img/logo.png';
+import {useFonts} from 'expo-font'
 
 
 export default function App() {
+
+const [fontsLoaded]=useFonts({'Inter-Medium':require('./assets/fonts/Inter-Medium.ttf')});
+  
+   
   return (
     //Campo email
     <View style={estilos.container}>
+
+     <Image style={estilos.img} source={logoImage}/>
+       
+      
+  
+
+      
+
+
+       <Text style={estilos.fraseinic}>GOSTARIAMOS TE CONHECER MELHOR</Text>
        
       <Text style={estilos.texto}>
-        E-mail
+       
       </Text>
       <TextInput
-        style={estilos.input} />
+      placeholder="E-mail"
+      placeholderTextColor="#666666"
+        style={estilos.input}
+         />
 
+
+      {/* Campo senha */}
       <Text style={estilos.texto}>
-        Senha
+  
       </Text>
       <TextInput
         style={estilos.input}
-        secureTextEntry={true} />
+        placeholder="Senha"
+        placeholderTextColor="#666666"
+        secureTextEntry={true}
+        />
 
-
+{/* Campo nome */}
       <Text style={estilos.texto}>
-        Como prefere ser chamado:
+        
       </Text>
       <TextInput
+        placeholder="Nome"
+        placeholderTextColor="#666666"
         style={estilos.input} />
 
-
+{/*Data de nascimento*/}
       <Text style={estilos.texto}>
-        Informe sua idade:
+      
       </Text>
       <TextInput
+        placeholder="Data de nascimento"
+        placeholderTextColor="#666666"
+        
         style={estilos.input}
         keyboardType="number-pad" />
 
@@ -58,14 +85,9 @@ const estilos = StyleSheet.create({
     backgroundColor: '#FCFAFF',
     alignItems: 'center',
     justifyContent: 'center',
-    gap: 8
+    gap: 12
   },
-  //texto acima das caixas
-  texto: {
-    color: "#000",
-    fontSize: 12,
-
-  },
+  
   //texto que vai dentro da caixa
   input: {
     borderWidth: 1,
@@ -75,18 +97,33 @@ const estilos = StyleSheet.create({
     width: "80%",
     backgroundColor:'#EBE8F8',
     borderRadius: 20,
+    borderColor:'#ddd6f0',
+    fontFamily:'Inter-Medium'
   },
   button: {
     backgroundColor: "#110E26",
     paddingHorizontal: 80,
     paddingVertical: 16,
     borderRadius: 28,
-    marginTop:12,
-    
-    
+    marginTop:50,
   },
   textobotao: {
-    color:"white"
+    color:"white",
+    fontWeight: '500',
+    fontFamily:'Inter-Medium',
+     
+  },
+  fraseinic:{
+    color: "#110E26",
+    fontWeight: '500',
+    fontFamily:'Inter-Medium', 
+    
+  },
+  img:{
+    width:"150",
+    height:"400",
+    padding:50,
+    marginBottom:50,
   }
 });
 
