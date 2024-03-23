3e407a4c7b76855f1689c41ab85952d906808113
TaliaDiasDuarte <taliadias.adm@gmail.com>
2024-03-22 21:52:30 -0300

    alteracao4

--- App.js
+++ App.js
@@ -1,80 +1,156 @@
-
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Input ,Alert} from 'react-native';
 import logoImage from './src/img/logo.png';
-import {useFonts} from 'expo-font'
+import { useFonts } from 'expo-font'
+
+
 
 
 export default function App() {
+  //importador de fonte
+  const [] = useFonts({ 'Inter-Medium': require('./assets/fonts/Inter-Medium.ttf') });
 
-const [fontsLoaded]=useFonts({'Inter-Medium':require('./assets/fonts/Inter-Medium.ttf')});
+
+
+//recebendo as variaveis email
+  const [email, setEmail] = useState(null)
+  const [errorEmail, setErrorEmail] = useState(null)
+
+//recebendo variaveis senha
+  const [senha, setSenha] = useState(null)
+  const [errorSenha, setErrorSenha] = useState(null)
+
+
+
+  const validar = () => {
+    let error = false
+    setErrorEmail(null)
+
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (!re.test(String(email).toLowerCase())) {
+      setErrorEmail("Preencha seu e-mail corretamente");
+      console.log("Preencha seu e-mail corretamente");
+       
+      error = true
+    }
+    
+    
+
+    if(senha === null){
+      setErrorSenha("Preencha sua senha")
+      error = true
+      
+      console.log("Preencha seu senha");
+
+    }
+    return !error
+
+  }
+
+  const salvar = () => {
+    if (validar()) {
+      console.log("Salvou email");
+    }
+
+
+  }
+
+
   
-   
   return (
     //Campo email
     <View style={estilos.container}>
-
-     <Image style={estilos.img} source={logoImage}/>
-       
-      
-  
-
-      
+      {/* logo imagem */}
+      <Image style={estilos.img} source={logoImage} />
 
 
-       <Text style={estilos.fraseinic}>GOSTARIAMOS TE CONHECER MELHOR</Text>
-       
+      <Text style={estilos.fraseinic}>GOSTARIAMOS TE CONHECER MELHOR</Text>
+
       <Text style={estilos.texto}>
-       
+
       </Text>
+
+     
       <TextInput
-      placeholder="E-mail"
-      placeholderTextColor="#666666"
+        placeholder="E-mail"
+        placeholderTextColor="#666666"
         style={estilos.input}
-         />
+
+        onChangeText={value => {
+          setEmail(value)
+          setErrorEmail(null)
+          
+        }}
+        errorMessage={errorEmail} 
+        
+      />
+
+   <Text style={estilos.errorMessage}>{errorEmail} </Text> 
+       
+      
+      
+      
+
+
 
 
       {/* Campo senha */}
       <Text style={estilos.texto}>
-  
+
       </Text>
       <TextInput
         style={estilos.input}
         placeholder="Senha"
         placeholderTextColor="#666666"
         secureTextEntry={true}
-        />
 
-{/* Campo nome */}
+        onChangeText={value => {setSenha(value);
+          setErrorSenha(null);
+          
+      }}/>
+      
+      <Text style={estilos.errorMessage}>{errorSenha} </Text>
+
+
+      {/* Campo nome */}
       <Text style={estilos.texto}>
-        
       </Text>
       <TextInput
         placeholder="Nome"
         placeholderTextColor="#666666"
-        style={estilos.input} />
+        style={estilos.input}
+      />
 
-{/*Data de nascimento*/}
+
+
+      {/*Data de nascimento*/}
       <Text style={estilos.texto}>
-      
+
       </Text>
       <TextInput
         placeholder="Data de nascimento"
         placeholderTextColor="#666666"
-        
+
         style={estilos.input}
-        keyboardType="number-pad" />
+        keyboardType="number-pad"
+        />
 
 
 
 
-     
+
+      {/* style={estilos.button} */}
+      <TouchableOpacity style={estilos.button}
+
+        onPress={() => salvar()}>
 
 
-      <TouchableOpacity style={estilos.button}>
+
         <Text style={estilos.textobotao} >
           CADASTRAR
         </Text>
       </TouchableOpacity>
+
     </View>
   );
 }
@@ -85,9 +161,9 @@ const estilos = StyleSheet.create({
     backgroundColor: '#FCFAFF',
     alignItems: 'center',
     justifyContent: 'center',
-    gap: 12
+    gap:4
   },
-  
+
   //texto que vai dentro da caixa
   input: {
     borderWidth: 1,
@@ -95,35 +171,41 @@ const estilos = StyleSheet.create({
     paddingVertical: 8,
     borderRadius: 8,
     width: "80%",
-    backgroundColor:'#EBE8F8',
+    backgroundColor: '#EBE8F8',
     borderRadius: 20,
-    borderColor:'#ddd6f0',
-    fontFamily:'Inter-Medium'
+    borderColor: '#ddd6f0',
+    fontFamily: 'Inter-Medium'
   },
   button: {
     backgroundColor: "#110E26",
     paddingHorizontal: 80,
     paddingVertical: 16,
     borderRadius: 28,
-    marginTop:50,
+    marginTop: 50,
   },
   textobotao: {
-    color:"white",
+    color: "white",
     fontWeight: '500',
-    fontFamily:'Inter-Medium',
-     
+    fontFamily: 'Inter-Medium',
+
   },
-  fraseinic:{
+  fraseinic: {
     color: "#110E26",
     fontWeight: '500',
-    fontFamily:'Inter-Medium', 
-    
+    fontFamily: 'Inter-Medium',
+
   },
-  img:{
-    width:"150",
-    height:"400",
-    padding:50,
-    marginBottom:50,
+  img: {
+    width: "150",
+    height: "400",
+    padding: 50,
+    marginBottom: 50,
+  },
+  errorMessage:{
+    color: "#524e81",
+    fontSize: 12,
+    backgroundColor: '#FCFAFF',
+    fontFamily: 'Inter-Medium',
   }
 });
 
